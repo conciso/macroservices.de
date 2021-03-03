7a8ba2ef150374d401335b7e94aba772c8e4135e
Sven-Torben Janus <sven-torben.janus@conciso.de>
2021-03-03 08:50:22 +0100

    Reduce max-width on large devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@
 (function($) {
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
+		xlarge: '(max-width: 980px)',//xlarge: '(max-width: 1680px)',
+		large: '(max-width: 980px)',//large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
 		xsmall: '(max-width: 480px)',
